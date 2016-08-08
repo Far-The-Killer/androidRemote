832944da1ef430b6a1d38df43b72a26e49a28e89
Justin Foehner <jmf9808@rit.edu>
2016-08-07 23:30:31 -0400

    Initial commit.

---
+++ Remote/MainActivity.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Android.App;
+using Android.Bluetooth.LE;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace Remote
+{
+    [Activity(Label = "Remote", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        Button playBtn;
+        bool paused;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            playBtn = FindViewById<Button>(Resource.Id.playBtn);
+            playBtn.Click += SendPlay;
+        }
+
+        public void SendPlay(Object sender ,EventArgs e)
+        {
+            paused = !paused;
+
+            if(paused)
+            {
+                playBtn.Background = GetDrawable(Resource.Drawable.pauseBtn_Select);
+                playBtn.Rotation = 0.0f;
+            }
+            else
+            {
+                playBtn.Background = GetDrawable(Resource.Drawable.playBtn_Select);
+                playBtn.Rotation = 90.0f;
+            }
+        }
+    }
+}
+
